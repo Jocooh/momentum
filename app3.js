14d089da92fbe1b397fbfe561e5e2c8b99d091d8
seounga <tjddkwh96@naver.com>
2022-08-21 21:24:19 +0900

    https://github.com/CHOSEOUNGA/momentum.git

---
+++ app3.js
@@ -0,0 +1,9 @@
+/* const age =prompt("How old are you?");
+console.log(typeof age);
+
+console.log(typeof "15",typeof parseInt("15")); */
+// ㄴstring을 number로 바꿔줌,, parseInt()
+
+const age=parseInt(prompt("How old are you?"));
+console.log(age);
+
